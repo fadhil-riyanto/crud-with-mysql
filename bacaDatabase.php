ed826df57f2320defe456940c67a4803b0f228c3
internsial-node <orangasingtakkenal@gmail.com>
2020-09-19 15:12:33 +0700

    Buat css dan ubah tampilan antar muka

--- bacaDatabase.php
+++ bacaDatabase.php
@@ -16,62 +16,69 @@ $result = mysqli_query($koneksi, "SELECT * FROM `users` ORDER BY `id` DESC");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="gaya_crud.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap\css\bootstrap.css">
 </head>
 
 <body>
-    <h1>Daftar teman</h1>
-    <hr>
-    <a href="Tambah.php">Tambah data</a> | <a href="logout.php">Logout</a>
-    <div id="keyword">
+
+
+
+
+
+    <br>
+    <a href="Tambah.php" class='tombol'>Tambah data</a> | <a href="logout.php" class='tombol'>Logout</a>
+    <br><br><br>
+    <!-- <div id="keyword">
         <input type="text" name="keyword" placeholder="Cari data...!">
-    </div>
-
-    <div id="container">
-        <table border="1">
-            <tr>
-                <th>
-                    No
-                </th>
-                <th>
-                    Aksi For admin
-                </th>
-                <th>
-                    Nama
-                </th>
-                <th>
-                    email
-                </th>
-                <th>
-                    no whatsapp
-                </th>
-            </tr>
-            <?php $i=1;?>
-            <?php while ($ambil_result = mysqli_fetch_assoc($result)):?>
+    </div> -->
 
 
-            <tr>
-                <td>
-                    <?= $i?>
-                </td>
-                <td>
-                    <a href="hapus.php?id=<?= $ambil_result["id"]?>">Hapus</a> | <a
-                        href="ubah.php?id=<?= $ambil_result["id"]?>">Ubah</a>
-                </td>
-                <td>
-                    <?= $ambil_result["name"]?>
-                </td>
-                <td>
-                    <?= $ambil_result["email"]?>
-                </td>
-                <td>
-                    <?= $ambil_result["no_whatsapp"]?>
-                </td>
-            </tr>
-            <?php $i++?>
-            <?php endwhile;?>
+    <table class="table1">
+        <tr>
+            <th>
+                No
+            </th>
+            <th>
+                Aksi For admin
+            </th>
+            <th>
+                Nama
+            </th>
+            <th>
+                email
+            </th>
+            <th>
+                no whatsapp
+            </th>
+        </tr>
+        <?php $i=1;?>
+        <?php while ($ambil_result = mysqli_fetch_assoc($result)):?>
 
-        </table>
-    </div>
+
+        <tr>
+            <td>
+                <?= $i?>
+            </td>
+            <td>
+                <a href="hapus.php?id=<?= $ambil_result["id"]?>">Hapus</a> | <a
+                    href="ubah.php?id=<?= $ambil_result["id"]?>">Ubah</a>
+            </td>
+            <td>
+                <?= $ambil_result["name"]?>
+            </td>
+            <td>
+                <?= $ambil_result["email"]?>
+            </td>
+            <td>
+                <?= $ambil_result["no_whatsapp"]?>
+            </td>
+        </tr>
+        <?php $i++?>
+        <?php endwhile;?>
+
+    </table>
+
     <script>
 
         var keyword = documentGetElementById('keyword');
@@ -93,6 +100,12 @@ $result = mysqli_query($koneksi, "SELECT * FROM `users` ORDER BY `id` DESC");
         });
 
     </script>
+
+
 </body>
+<footer>
+    <br><br><br>
+    <center>script &copy Internsial Node</center>
+</footer>
 
 </html>
--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
 if(isset($_GET["pesan"])){
     if ($_GET["pesan"] == "gagal"){
-        echo "Login gagal! Username dan password salah";
+        echo "
+        
+        <div id='myModal' class='modal fade' role='dialog'>
+            <div class='modal-dialog'>
+        
+                <!-- Modal content-->
+                <div class='modal-content'>
+                    <div class='modal-header'>
+                        <button type='button' class='close' data-dismiss='modal'>&times;</button>
+                        <h4 class='modal-title'>Modal Header</h4>
+                    </div>
+                    <div class='modal-body'>
+                        <p>Some text in the modal.</p>
+                    </div>
+                    <div class='modal-footer'>
+                        <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
+                    </div>
+                </div>
+        
+            </div>
+        </div>
+        
+        ";
     } elseif ($_GET["pesan"] == "logout"){
         echo "anda sudah logout";
     } elseif ($_GET["pesan"] == "belum login") {
@@ -11,22 +33,29 @@ if(isset($_GET["pesan"])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="gaya_login.css">
+
+
 </head>
+
 <body>
-    <h1>Login Admin</h1>
-<hr>
-    <form action="cek_login.php" method="post">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username" required="yes" autocomplete="off">
-            <br>
-        <label for="password">password</label>
-        <input type="password" name="password" id="password" required="yes" autocomplete="off">
-            <br>
+
+
+    <form action="cek_login.php" method="post" class="kotak">
+        <h1>Login Here</h1>
+
+        <input type="text" name="username" id="username" required="yes" autocomplete="off" placeholder="Username">
+        <br>
+
+        <input type="password" name="password" id="password" required="yes" autocomplete="off" placeholder="Password">
+        <br>
         <button type="submit" name="submit">Kirim</button>
     </form>
 </body>
+
 </html>
