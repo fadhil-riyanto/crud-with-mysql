63a741be9301339ec2a099de7f5da48c24f51b04
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-09-20 13:19:42 +0700

    menambahkan boostrap dan javascript untuk membuat pop-up

--- bacaDatabase.php
+++ bacaDatabase.php
@@ -17,7 +17,9 @@ $result = mysqli_query($koneksi, "SELECT * FROM `users` ORDER BY `id` DESC");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" type="text/css" href="gaya_crud.css">
-    <link rel="stylesheet" type="text/css" href="bootstrap\css\bootstrap.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
 </head>
 
 <body>
@@ -25,9 +27,22 @@ $result = mysqli_query($koneksi, "SELECT * FROM `users` ORDER BY `id` DESC");
 
 
 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
 
     <br>
+
     <a href="Tambah.php" class='tombol'>Tambah data</a> | <a href="logout.php" class='tombol'>Logout</a>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+        Info developers
+    </button>
     <br><br><br>
     <!-- <div id="keyword">
         <input type="text" name="keyword" placeholder="Cari data...!">
@@ -100,6 +115,33 @@ $result = mysqli_query($koneksi, "SELECT * FROM `users` ORDER BY `id` DESC");
         });
 
     </script>
+
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Info developers</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <P>Nama : Fadhil Riyanto
+                    <p>Pekerjaan : Programmer</p>
+                    <p>Umur : 14 tahun</p>
+                    <p>Hobi : Tidur</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 </body>
